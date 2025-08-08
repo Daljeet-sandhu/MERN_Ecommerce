e76c1baf1d3a85ec959073154e539b2a3cafc7de
Daljeet Singh <61301173+Daljeet-sandhu@users.noreply.github.com>
2025-08-08 18:09:05 -0400

    Delete frontend directory

--- frontend/src/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-export const convertToINR = (number: number) => {
-  return new Intl.NumberFormat("en-IN", {
-    style: "currency",
-    currency: "INR",
-  })
-    .format(number)
-    .slice(0, -3);
-};
